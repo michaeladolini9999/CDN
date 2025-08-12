9cbfb500ec7de0aae25c2e56724348572775f13e
adolinimichael <adolinimichael@Op.local>
2025-08-12 23:01:25 +0700

    modified:   nginx.sh 	modified:   nginx/nginx1.conf

--- nginx.sh
+++ nginx.sh
@@ -46,18 +46,39 @@ OUT_DIR="/home/ubuntu/CDN/nginx/conf.d"
 # Đảm bảo thư mục tồn tại
 mkdir -p "$OUT_DIR"
 rm -f "$OUT_DIR"/*
-# Trích xuất danh sách cặp [server_name, origin] duy nhất
-jq -r '.apps[] | [. [0], .[1]] | @tsv' "$JSON_FILE" | sort -u | while read -r server_name origin; do
+# Trích xuất danh sách server_name duy nhất
+jq -r '.apps[] | [. [0]] | @tsv' "$JSON_FILE" | sort -u | while read -r server_name; do
 
   # Tạo wildcard từ server_name
   domain=$(echo "$server_name" | cut -d. -f2-)
   wildcard="wild.$domain"
 
+  search_pattern="listen 8080 default_server;"
+
+# Kiểm tra đã tồn tại chưa
+  if grep -qF "$search_pattern" "$NGINX_FILE"; then
+    echo "✅ 'listen 8080 default_server;' existed in $NGINX_FILE"
+  else
+    echo "⚠️  Adding default_server into block http { ... }"
+    sed -i '/http\s*{/,/^}/ {
+    /^}/ i\
+\tserver {\
+\t\tlisten 8080 default_server;\
+\t\tserver_name _;\
+\t\tssl_certificate /home/ubuntu/CDN/ssl/'"$wildcard"'/fullchain.pem;\
+\t\tssl_certificate_key /home/ubuntu/CDN/ssl/'"$wildcard"'/privkey.pem;\
+\t\tssl_trusted_certificate /home/ubuntu/CDN/ssl/'"$wildcard"'/chain.pem;\
+\t\treturn 404;\
+\t}
+}' "$NGINX_FILE"
+
+    echo "✅ Added block default_server into $NGINX_FILE"
+  fi
+
   # Tạo tên file
   conf_file="$OUT_DIR/$server_name.conf"
 
   cat >> "$conf_file" <<EOF
-
 server {
     listen 8080 ssl http2;
     server_name $server_name;
